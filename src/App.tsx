e094d3f47e8ffa6f81c7793152659e9a282e8fe5
joe888777 <joe888777@gmail.com>
2023-12-02 14:51:30 +0800

    print chainlink price feed on fe
    - test garbage mint method to mind garbage token
    - first step is to connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , ChangeEvent, useEffect, useRef } from 'react'
 import './App.css'
+import Web3 from 'web3';
+import { ChainlinkPlugin, GoerliPriceFeeds } from '@chainsafe/web3.js-chainlink-plugin';
+import { Button, Modal } from 'react-bootstrap';
+import {abi, address as contractAddress } from './contract';
+import { walletAddress } from './config';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [targetAddress, setTargetAddress] = useState("0x400627");
+    const [amountStr, setAmountStr] = useState("");
+    const [error, setError] = useState("");
+    const web3 = useRef<any>(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [priceShow, setPriceShow] = useState<any>(-1);
+    
+    const connectWallet = async () => {
+        if (window && window.ethereum) {
+            try {
+                await window.ethereum.request({ method: "eth_requestAccounts"});
+                web3.current = new Web3(window.ethereum);
+                const chainlinkPlugin = new ChainlinkPlugin();
+                web3.current.registerPlugin(chainlinkPlugin);
+                console.dir(web3.current);
+                setIsConnected(true);
+            } catch (err: Error) {
+                setError(err.message);
+                setIsConnected(false);
+            }
+            
+        } else {
+            alert("please install MetaMask");
+        }
+    }
+
+    const getPricefeed = (priceFeed: GoerliPriceFeeds) => {
+        web3.current.chainlink.getPrice(priceFeed).then((res: any) => {
+            console.dir(res);
+            setPriceShow(res.answer);
+        });
+    }
+
+    const walletMint = () => {
+        const contract = new web3.current.eth.Contract(abi, contractAddress);
+        console.dir(contract.methods);
+        contract.methods.mint(walletAddress, 1000000000).send({ from: walletAddress },
+            (err, txHash) => {
+                if (err) {
+                console.error(err);
+                } else {
+                console.log(txHash);
+                }
+            }
+        )
+    }
+
+  useEffect(() => {
+    //set address
+    console.dir(web3);
+  }, []);
+  useEffect(() => {
+    console.log(priceShow)
+  }, [priceShow])
+  
+  const handleSetNumber = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!isNaN(parseFloat(e.target.value))) {// FIXME
+      setAmountStr(e.target.value);
+    }
+  }
 
   return (
     <>
+        <div>
+        <h1>polyPay</h1>
+        transfer <input type="text" onChange={handleSetNumber} value={amountStr} /> usdt to {targetAddress}
+        <Button onClick={()=> {connectWallet(); }}>按下去連接錢包</Button>
+        <Button onClick={()=> {walletMint(); }}>mint MTK token</Button>
+        {isConnected &&(
+            <div>
+                <Button onClick={()=> {getPricefeed(GoerliPriceFeeds.BtcUsd); }}>get price feed BTC/USD</Button>
+                <Button onClick={()=> {getPricefeed(GoerliPriceFeeds.BtcEth); }}>get price feed BTC/ETH</Button>
+                <Button onClick={()=> {getPricefeed(GoerliPriceFeeds.EthUsd); }}>get price feed ETH/USD</Button>
+                <Button onClick={()=> {getPricefeed(GoerliPriceFeeds.DaiUsd); }}>get price feed Dai/USD</Button>
+            </div>
+        )}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <font color="red">
+            {error}
+        </font>
+        <font color="green">{`${priceShow}`}</font>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const walletAddress = "0x750ebea257a20F10217D46c914eea89094aa86d0";
